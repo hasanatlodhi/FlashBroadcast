b2ddae0a4fa89d3d7834c3d999989833b311297c
hasanatlodhi <lodhihasanat@gmail.com>
2020-02-28 12:58:17 +0500

    2nd Commit(FlashONCAll)

--- app/src/main/java/twenty20/example/flashbroadcast/MainActivity.kt
+++ app/src/main/java/twenty20/example/flashbroadcast/MainActivity.kt
@@ -1,22 +1,62 @@
 package twenty20.example.flashbroadcast
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.hardware.Camera
 import android.hardware.camera2.CameraManager
 import android.net.ConnectivityManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
 import java.lang.reflect.Parameter
 
 public class MainActivity : AppCompatActivity() {
+    private val PERMISSION_CODE = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED &&checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED)
+            {
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_PHONE_STATE)
+                requestPermissions(permission, PERMISSION_CODE)
+            }
+            else{
+                text.setText("You Have Granted Permissions")
+
+            }
+        }
+        else
+        {
+            text.setText("You Have Granted Permissions")
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    var toast=Toast.makeText(this,"Pemissions Granted For Flash!",Toast.LENGTH_SHORT)
+                    toast.setGravity(Gravity.CENTER,0,0)
+                    toast.show()
+                    text.setText("You Have Granted Permissions!")
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    text.setText("Please Enable the Permissions From Settings")
+                }
+            }
+        }
+         }
 }
--- app/src/main/java/twenty20/example/flashbroadcast/MyReceiver.kt
+++ app/src/main/java/twenty20/example/flashbroadcast/MyReceiver.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.hardware.Camera
 import android.hardware.camera2.CameraManager
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
@@ -18,14 +17,14 @@ import java.security.Policy
 import java.util.jar.Manifest
 
 @Suppress("DEPRECATION")
-public class MyReceiver(): BroadcastReceiver(){
-        override fun onReceive(context: Context, intent: Intent) {
+public class MyReceiver(): BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
 
-            var cameram= context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-            var cameraid=cameram.cameraIdList[0]
-            var myString:String = "010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010"
-            var blinkdelay:Long=50
-            var state: String = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+        var cameram = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        var cameraid = cameram.cameraIdList[0]
+        var myString: String = "0101010101010101010101010101010101010101010101010101010101010101"
+        var blinkdelay: Long = 50
+        var state: String = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
         if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
             Toast.makeText(context, "Ringing", Toast.LENGTH_SHORT).show()
             for (i in 0 until myString.length) {
@@ -34,25 +33,30 @@ public class MyReceiver(): BroadcastReceiver(){
                 } else {
                     cameram.setTorchMode(cameraid, false)
                 }
-                try {
-                    Thread.sleep(blinkdelay)
+                if (state != TelephonyManager.EXTRA_STATE_IDLE && state != TelephonyManager.EXTRA_STATE_OFFHOOK) {
+                    try {
+                        Thread.sleep(blinkdelay)
+                        continue
 
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    cameram.setTorchMode(cameraid, false)
+                    break
                 }
             }
         }
-            if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+           else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
                 cameram.setTorchMode(cameraid, false)
-                Toast.makeText(context, "Call Received", Toast.LENGTH_SHORT).show()
-            }
-            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                  }
+            else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
                 cameram.setTorchMode(cameraid, false)
-                Toast.makeText(context, "Phone is Idle", Toast.LENGTH_SHORT).show()
-            }
+                }
 
-        }
+
     }
+}
 
 
 
